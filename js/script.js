d2f083e83462802a0976b4b8e1e87158b8453a54
kanonir14 <kanonirs@yandex.ru>
2016-06-11 11:57:21 +0300

    Update 1.1.5

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	accordion();
+	nav();
 });
 
 // sidebar accordion
@@ -9,4 +10,27 @@ function accordion() {
 			$(this).children('.dropdown-ul').toggle(300);
 		}
 	});
+}
+
+//navigation
+function nav() {
+	var hamburger = $('.hamburger'),
+		nav = $('.nav-ul');
+
+	hamburger.on({
+		click: function(){
+
+			if ( $(this).hasClass('active') ) {
+				nav.animate({right: '0px'}, 300);
+				hamburger.css('position', 'relative').animate({left: '-250px'}, 300);
+				$(this).removeClass('active');
+			} else {
+				nav.animate({right: '-250px'}, 300);
+				hamburger.css('position', 'relative').animate({left: '0px'}, 300);
+				$(this).addClass('active');
+			}
+			
+		}
+	});
+
 }
